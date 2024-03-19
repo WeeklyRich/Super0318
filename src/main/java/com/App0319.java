d464a00b292108d284aa8da66b51c5010e54b33e
zhou1094448282@163.com <zhou1094448282@163.com>
2024-03-19 15:34:18 +0800

    可以使用

--- src/main/java/com/App0319.java
+++ src/main/java/com/App0319.java
@@ -1,20 +1,14 @@
-package org.example;
+package com;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
 @MapperScan (basePackages = {"com.mapper"})
-public class App
-{
-
+public class App0319 {
     public static void main( String[] args )
     {
-        SpringApplication.run( App.class , args );
+        SpringApplication.run( App0319.class , args );
     }
 }
